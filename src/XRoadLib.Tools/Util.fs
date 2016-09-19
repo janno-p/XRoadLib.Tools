3b14680735b7adcd77710f64558ed7a56fb9a235
Janno Põldma <janno.poldma@gmail.com>
2016-09-20 00:06:00 +0300

    Missing parts transferred.

--- src/XRoadLib.Tools/Util.fs
+++ src/XRoadLib.Tools/Util.fs
@@ -1,5 +1,6 @@
 module XRoadLib.Tools.Util
 
+open System
 open System.IO
 open System.Xml.Linq
 open XRoadLib
@@ -25,3 +26,32 @@ module Seq =
 [<AutoOpen>]
 module Operators =
     let (</>) a b = Path.Combine(a, b)
+
+type XElement with
+    member this.HasAttribute(attributeName) =
+        this.Attribute(XName.Get(attributeName)) |> isNotNull
+
+[<AutoOpen>]
+module XElement =
+    let hasAttribute attributeName (element: XElement) = element.HasAttribute(attributeName)
+
+type XElementParser(element: XElement, ?ns) as this =
+    let ns = defaultArg ns NamespaceConstants.XSD
+    let mutable isDone = true
+    let enumerator = element.Elements().GetEnumerator()
+    let moveToNext () = match enumerator.MoveNext() with true -> isDone <- false; true | x -> x
+    member __.IsDone with get() = isDone
+    member __.AttributeNotImplemented(attributeName) =
+        if element |> hasAttribute attributeName then attributeName |> notImplemented
+    member __.ParseElement(name, ?action: XElement -> unit) =
+        if isDone && (moveToNext() |> not) then false
+        elif enumerator.Current |> isNotNull && enumerator.Current.Name = XName.Get(name, ns) then
+            if action |> Option.isNone then enumerator.Current.Name.ToString() |> notImplemented
+            action |> Option.iter (fun f -> f(enumerator.Current))
+            isDone <- true
+            true
+        else false
+    member __.ThrowIfNotDone() =
+        if not isDone then enumerator.Current.Name.ToString() |> notImplemented
+    interface IDisposable with
+        member __.Dispose() = this.ThrowIfNotDone()
